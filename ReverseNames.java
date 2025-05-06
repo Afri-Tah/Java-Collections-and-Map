77df0e4e807705cb13a26aee1210dd29a4e64970
Afrida Tahsin <atworkafrida@gmail.com>
2025-05-06 16:53:24 +0600

    Add files via upload

---
+++ ReverseNames.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+//Take a list of student names, and print them in reverse order.
+public class ReverseNames {
+    public static void main(String[] args) {
+        List<String>names=new ArrayList<>(Arrays.asList("Afrida","Tahsin","Tahmid","Akib"));
+       for(int i=names.size()-1;i>=0;i--){
+           System.out.print(names.get(i)+" ");
+        }
+
+    }
+}
